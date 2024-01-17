d4304c143bed8c8823e9a40290feeab60c614499
murban27 <murban27@protonmail.com>
2024-01-17 21:54:32 +0100

    commit
    init

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -4,7 +4,7 @@ import 'package:mealsapp/models/category.dart';
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({super.key, required this.category});
     final Category category;
-
+//
   @override
   Widget build(BuildContext context) {
     return 
